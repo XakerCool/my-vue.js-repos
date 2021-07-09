8d56ec04cc5b06fb27d8a9bf18f2d57fe395c14d
Hakknazar <68599654+XakerCool@users.noreply.github.com>
2021-07-09 23:28:08 +0600

    1 homework

---
+++ 1_hm/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ModalWindow/>
+  </div>
+</template>
+
+<script>
+import ModalWindow from '@/components/ModalWindow.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ModalWindow
+  }
+}
+</script>
+
---
+++ 1_hm/ModalWindow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="modal" v-if="isModalOpen">
+        <p>Мы используем куки, примите во внимание!</p>
+        <button @click="closeModal">Закрыть</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isModalOpen: true
+        }
+    },
+    methods: {
+        closeModal() {
+            this.isModalOpen = false;
+        }
+    }
+}
+</script>
